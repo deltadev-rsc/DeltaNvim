2b26d3996e9d7592df0fa6577b5cc653c194c65c
deltadev-rsc <deltaqxq174@mail.ru>
2026-04-15 20:46:27 +0500

    Update lsp.lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -17,4 +17,40 @@ vim.lsp.config('rust_analyzer', {
 })
 
 
-vim.lsp.config('clangd', {})
+vim.lsp.config('clangd', {
+    cmd = { "clangd" },
+    filetypes = {
+        "c",
+        "cpp",
+        "objc",
+        "objcpp",
+        "cuda"
+    },
+    
+    root_markers = {
+        ".clangd",
+        ".clangd-tidy",
+        ".clangd-format",
+        "compile_commands.json",
+        "compile_flags.txt",
+        "configure.ac",
+        ".git"
+    },
+
+    capabilities = {
+        offsetEncoding = {
+            "utf-8",
+            "utf-16"
+        },
+        textDocument = {
+            completion = {
+                editsNearCursor = true
+            }
+        }
+    }
+})
+
+vim.lsp.config('bash-language-server', {
+    cmd = { 'bash-language-server', 'start' },
+    filetypes = { 'bash', 'sh' },
+})
