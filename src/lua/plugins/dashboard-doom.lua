e0093de7c0c93e86a0ca329660c074b72b0b0f82
deltadev-rsc <deltaqxq174@mail.ru>
2026-05-13 21:40:31 +0500

    Create dashboard-doom.lua

---
+++ src/lua/plugins/dashboard-doom.lua
@@ -0,0 +1,72 @@
+require('dashboard').setup({
+    theme = 'doom',
+    config = {
+        header = {
+                '',
+                '    ███            ███████        ',
+                '   ███ ███        ███    ███      ',
+                '   ███  ███       ███     ███     ',
+                '   ███   ███      ███      ███    ',
+                '   ███    ███     ███       ███   ',
+                '   ███     ███    ███        ███  ',
+                '   ███      ███   ███        ███  ',
+                '   ███       ███  ███        ███  ',
+                '   ███        ███ ███        ███  ',
+                '   ███         ██████       ███   ',
+                '    ██           ███ ████████     ',
+                '',
+        },
+        center = {
+            {
+                icon = ' ',
+                icon_hl = 'Title',
+                desc = 'Find File           ',
+                desc_hl = 'String',
+                key = 'f',
+                key_hl = 'Number',
+                key_format = ' %s', -- remove default surrounding `[]`
+                action = 'Telescope find_files'
+            },
+            {
+                icon = ' ',
+                desc = 'Open Terminal',
+                key = 't',
+                key_format = ' %s', -- remove default surrounding `[]`
+                action = 'ToggleTerm direction=float'
+            },
+            {
+                icon = '󰈆 ',
+                desc = 'Quit',
+                key = 'q',
+                key_format = ' %s',
+                action = 'quit'
+            },
+            {
+                icon = '󱪝 ',
+                desc = 'New File',
+                key = 'n',
+                key_format = ' %s',
+                action = 'enew'
+            },
+            {
+                icon = '󱧷 ',
+                desc = 'Recent Files',
+                key = 'r',
+                key_format = ' %s',
+                action = 'Telescope oldfiles'
+            },
+            {
+                icon = ' ',
+                desc = 'Plugin Manager',
+                key = 'p',
+                key_format = ' %s',
+                action = 'Lazy'
+            }, 
+        },
+        footer = function()
+            local stats = require("lazy").stats()
+            local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+            return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
+        end, 
+    }
+})
