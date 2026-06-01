48a0765454887e76defc328ba96637806ecfc3a2
deltadev-rsc <deltaqxq174@mail.ru>
2026-06-01 18:43:07 +0500

    Update cmp.lua

--- src/lua/plugins/cmp.lua
+++ src/lua/plugins/cmp.lua
@@ -1,26 +1,29 @@
 local cmp = require'cmp'
 
-  cmp.setup({
+cmp.setup({
     snippet = {
-      expand = function(args)
-        vim.fn["vsnip#anonymous"](args.body) 
-      end,
+        expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body) 
+        end,
     },
+    
     window = {
-      completion = cmp.config.window.bordered(),
-      documentation = cmp.config.window.bordered(),
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
     },
+    
     mapping = cmp.mapping.preset.insert({
-      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.abort(),
-      ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<Tab>'] = cmp.mapping.confirm({ select = true }),
     }),
+
     sources = cmp.config.sources({
-      { name = 'nvim_lsp' },
-      { name = 'vsnip' }, 
-      }, {
-      { name = 'buffer' },
+        { name = 'nvim_lsp' },
+        { name = 'vsnip' }, 
+        }, {
+        { name = 'buffer' },
     })
-  })
+})
