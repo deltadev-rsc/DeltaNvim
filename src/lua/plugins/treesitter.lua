e8f6c3a06ba03657686be6ebb53218acaf336225
deltadev-rsc <deltaqxq174@mail.ru>
2026-06-01 18:43:48 +0500

    Update treesitter.lua

--- src/lua/plugins/treesitter.lua
+++ src/lua/plugins/treesitter.lua
@@ -1,7 +1,15 @@
 local ts = require('nvim-treesitter.configs')
 
 ts.setup {
-    ensure_installed = { 'lua', 'c', 'cpp', 'make', 'rust', 'bash', 'nasm' }, 
+    ensure_installed = { 
+        'lua', 
+        'c', 'cpp', 
+        'make', 
+        'rust', 
+        'bash', 
+        'nasm' 
+    }, 
+    
     auto_install = true,
     highlight = {
         enable = true,
