7c437f3faa17d1dea4fd2dfd5a8e06d61fc2becf
deltadev-rsc <deltaqxq174@mail.ru>
2026-06-11 12:06:40 +0500

    Create everf.lua

---
+++ src/lua/themes/everf.lua
@@ -0,0 +1,20 @@
+require("everforest").setup({
+    background = "hard",
+    transparent_background_level = 0,
+    italics = true,
+    disable_italic_comments = false,
+    sign_column_background = "none",
+    ui_contrast = "low",
+    dim_inactive_windows = false,
+    diagnostic_text_highlight = true,
+    diagnostic_virtual_text = "coloured",
+    diagnostic_line_highlight = false,
+    spell_foreground = false,
+    show_eob = true,
+    float_style = "bright",
+    inlay_hints_background = "none",
+    on_highlights = function(highlight_groups, palette) end,
+    colours_override = function(palette) end,
+})
+
+vim.cmd([[colorscheme everforest]])
