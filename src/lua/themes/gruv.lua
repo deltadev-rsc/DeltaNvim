765198b2dcbde6a5140ddcfbb16590ee5e3b1af7
deltadev-rsc <deltaqxq174@mail.ru>
2026-06-11 12:06:55 +0500

    Create gruv.lua

---
+++ src/lua/themes/gruv.lua
@@ -0,0 +1,51 @@
+-- For gruvbox and gruvbox-material
+
+require('gruvbox-material').setup({
+    italics = true,             -- enable italics in general
+    contrast = "hard",        -- set contrast, can be any of "hard", "medium", "soft"
+    
+    comments = {
+        italics = true,           -- enable italic comments
+    },
+    
+    background = {
+        transparent = false,      -- set the background to be opaque
+    },
+    
+    float = {
+        force_background = false, -- set to true to force backgrounds on floats even when
+        background_color = nil,   -- set color for float backgrounds. If nil, uses the default color set
+    },
+  
+    signs = {
+        force_background = false, -- set to true to force backgrounds on signs even when
+        background_color = nil,   -- set color for sign backgrounds. If nil, uses the default color set
+    },
+    customize = nil,
+})
+
+require("gruvbox").setup({
+    terminal_colors = false, 
+    undercurl = false,
+    underline = true,
+    bold = false,
+    italic = {
+        strings = true,
+        emphasis = true,
+        comments = true,
+        operators = false,
+        folds = true,
+    },
+    strikethrough = false,
+    invert_selection = false,
+    invert_signs = false,
+    invert_tabline = false,
+    inverse = false, 
+    contrast = "medium", 
+    palette_overrides = {},
+    overrides = {},
+    dim_inactive = true,
+    transparent_mode = false,
+})
+
+vim.cmd([[colorscheme gruvbox]])
