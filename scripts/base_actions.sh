a48dcc6ac60852719ad3b6c079f6c0364f69184d
deltadev-rsc <deltaqxq174@mail.ru>
2026-07-09 20:34:25 +0500

    Update base_actions.sh

--- scripts/base_actions.sh
+++ scripts/base_actions.sh
@@ -16,10 +16,10 @@ function base_actions {
             
             find ~/.config/ -type d -name nvim
             if [ -d "$NVIM_CONF" ]; then
+                rm -f ~/DeltaNvim/src/lazy-lock.json
                 mkdir -p "$NVIM_CONF_BACKUP"
                 cp -r -a "$NVIM_CONF" "$NVIM_CONF_BACKUP"
                 mv "$NVIM_CONF_FROM_REPO" "$NVIM_CONF"
-
             elif [ ! -d "$NVIM_CONF" ]; then
                 echo "у тебя нет папки с конфигом неовима"
                 echo "просто создай её по команде 'mkdir ~/.config/nvim' "
